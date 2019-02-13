088bac9ec1e825857f089f36e46757a8a9239796
DeadlyMC <modding.deadlymc@gmail.com>
2019-02-13 22:18:30 +0530

    Mod Dev files

---
+++ src/main/java/quickcarpet/QuickCarpet.java
@@ -0,0 +1,11 @@
+package quickcarpet;
+
+import net.fabricmc.api.ModInitializer;
+
+public class QuickCarpet implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
