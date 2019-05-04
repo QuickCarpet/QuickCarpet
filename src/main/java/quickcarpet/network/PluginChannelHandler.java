efd34647c65abda654906abf43c5338ce1dfaedc
Simon Wanner <skyrising@pvpctutorials.de>
2019-05-04 22:20:16 +0200

    Port plugin message networking code from 1.12
    (cherry picked from commit 56580db57a60b3188c50d467289e27a13670ac2f)

---
+++ src/main/java/quickcarpet/network/PluginChannelHandler.java
@@ -0,0 +1,19 @@
+package quickcarpet.network;
+
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.network.packet.CustomPayloadC2SPacket;
+import net.minecraft.util.Identifier;
+
+public interface PluginChannelHandler {
+    Identifier[] getChannels();
+
+    default boolean register(Identifier channel, ServerPlayerEntity player) {
+        return true;
+    }
+
+    default void unregister(Identifier channel, ServerPlayerEntity player) {
+
+    }
+
+    void onCustomPayload(CustomPayloadC2SPacket packet, ServerPlayerEntity player);
+}
---
+++ src/main/java/quickcarpet/network/PluginChannelManager.java
@@ -0,0 +1,131 @@
+package quickcarpet.network;
+
+import com.google.common.base.Charsets;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import net.minecraft.client.network.packet.CustomPayloadS2CPacket;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.PlayerManager;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.network.packet.CustomPayloadC2SPacket;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.PacketByteBuf;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import quickcarpet.mixin.ICustomPayloadC2SPacket;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class PluginChannelManager {
+    public static final Identifier REGISTER = new Identifier("minecraft:register");
+    public static final Identifier UNREGISTER = new Identifier("minecraft:unregister");
+    public static final Logger LOG = LogManager.getLogger();
+
+    private final MinecraftServer server;
+    public final PluginChannelTracker tracker;
+    private Map<Identifier, PluginChannelHandler> channelHandlers = new HashMap<>();
+
+    public PluginChannelManager(MinecraftServer server) {
+        this.server = server;
+        this.tracker = new PluginChannelTracker(server);
+    }
+
+    public void register(PluginChannelHandler handler) {
+        Identifier[] channels = handler.getChannels();
+        for (Identifier channel : channels) {
+            channelHandlers.put(channel, handler);
+        }
+        PlayerManager playerList = server.getPlayerManager();
+        // make sure server started up
+        if (playerList != null) {
+            sendChannelUpdate(playerList.getPlayerList(), REGISTER, Arrays.asList(channels));
+        }
+    }
+
+    public void unregister(PluginChannelHandler handler) {
+        Identifier[] channels = handler.getChannels();
+        for (Identifier channel : channels) {
+            for (ServerPlayerEntity player : tracker.getPlayers(channel)) {
+                handler.unregister(channel, player);
+                tracker.unregister(player, channel);
+            }
+            channelHandlers.remove(channel);
+        }
+        sendChannelUpdate(server.getPlayerManager().getPlayerList(), UNREGISTER, Arrays.asList(channels));
+    }
+
+    public void process(ServerPlayerEntity player, CustomPayloadC2SPacket packet) {
+        ICustomPayloadC2SPacket packetAccessor = (ICustomPayloadC2SPacket) packet;
+        Identifier channel = packetAccessor.getChannel();
+        PacketByteBuf payload = packetAccessor.getData();
+        switch(channel.toString()) {
+            case "minecraft:register": {
+                this.processRegister(player, payload);
+                return;
+            }
+            case "minecraft:unregister": {
+                this.processUnregister(player, payload);
+                return;
+            }
+        }
+        PluginChannelHandler handler = channelHandlers.get(channel);
+        if (handler != null) {
+            handler.onCustomPayload(packet, player);
+        }
+    }
+
+    private void processRegister(ServerPlayerEntity player, PacketByteBuf payload) {
+        List<Identifier> channels = getChannels(payload);
+        for (Identifier channel : channels) {
+            PluginChannelHandler handler = channelHandlers.get(channel);
+            if (handler != null && handler.register(channel, player)) {
+                tracker.register(player, channel);
+            }
+        }
+    }
+
+    private void processUnregister(ServerPlayerEntity player, PacketByteBuf payload) {
+        List<Identifier> channels = getChannels(payload);
+        for (Identifier channel : channels) {
+            if (!tracker.isRegistered(player, channel)) continue;
+            PluginChannelHandler handler = channelHandlers.get(channel);
+            if (handler != null) handler.unregister(channel, player);
+            tracker.unregister(player, channel);
+        }
+    }
+
+    public void onPlayerConnected(ServerPlayerEntity player) {
+        sendChannelUpdate(Collections.singleton(player), REGISTER, channelHandlers.keySet());
+    }
+
+    public void onPlayerDisconnected(ServerPlayerEntity player) {
+        for (Identifier channel : tracker.getChannels(player)) {
+            PluginChannelHandler handler = channelHandlers.get(channel);
+            if (handler == null) {
+                LOG.warn("Player was registered to channel '%s' without a handler", channel);
+                continue;
+            }
+            handler.unregister(channel, player);
+        }
+        tracker.unregisterAll(player);
+    }
+
+    private void sendChannelUpdate(Collection<ServerPlayerEntity> players, Identifier updateType, Collection<Identifier> channels) {
+        if (players.isEmpty()) return;
+        String joinedChannels = channels.stream().map(Identifier::toString).collect(Collectors.joining("\0"));
+        ByteBuf payload = Unpooled.wrappedBuffer(joinedChannels.getBytes(Charsets.UTF_8));
+        CustomPayloadS2CPacket packet = new CustomPayloadS2CPacket(updateType, new PacketByteBuf(payload));
+        for (ServerPlayerEntity player : players) {
+            player.networkHandler.sendPacket(packet);
+        }
+    }
+
+    private static List<Identifier> getChannels(PacketByteBuf buff) {
+        buff.resetReaderIndex();
+        byte[] bytes = new byte[buff.readableBytes()];
+        buff.readBytes(bytes);
+        String channelString = new String(bytes, Charsets.UTF_8);
+        return Arrays.stream(channelString.split("\0")).map(Identifier::new).collect(Collectors.toList());
+    }
+}
