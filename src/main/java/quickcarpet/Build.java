60d8600716ac6409c8d0e386187b5abb9b409087
DeadlyMC <modding.deadlymc@gmail.com>
2019-05-15 13:41:38 +0530

    Bump version to v1.10.0+skyblock.7

--- src/main/java/quickcarpet/Build.java
+++ src/main/java/quickcarpet/Build.java
@@ -2,5 +2,5 @@ package quickcarpet;
 
 public final class Build {
     public static final String NAME = "Quick Carpet";
-    public static final String VERSION = "1.9.1+skyblock.6";
+    public static final String VERSION = "1.10.0+skyblock.7";
 }
