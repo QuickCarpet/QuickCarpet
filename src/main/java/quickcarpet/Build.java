8b4a5b84c87ad478df64e4995e9fae7308d2ceca
Simon Wanner <skyrising@pvpctutorials.de>
2019-05-16 15:21:52 +0200

    Bump version to 1.10.1+skyblock.8

--- src/main/java/quickcarpet/Build.java
+++ src/main/java/quickcarpet/Build.java
@@ -2,5 +2,5 @@ package quickcarpet;
 
 public final class Build {
     public static final String NAME = "Quick Carpet";
-    public static final String VERSION = "1.10.0+skyblock.8";
+    public static final String VERSION = "1.10.1+skyblock.8";
 }
