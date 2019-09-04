4a374ad26d868ed126768c7c6d7c371ce25442fc
2No2Name <50278648+2No2Name@users.noreply.github.com>
2019-09-04 16:44:03 +0200

    Added movableBlockOverrides (#40)
    * Added movableBlockOverrides together with a new piston behavior: weak_sticky - blocks only stick to slime if they can move without any obstruction

--- src/main/java/quickcarpet/settings/Settings.java
+++ src/main/java/quickcarpet/settings/Settings.java
@@ -104,6 +104,9 @@ public class Settings {
     @Rule(category = {FEATURE, EXPERIMENTAL})
     public static boolean movableBlockEntities = false;
 
+    @Rule(category = {FEATURE,EXPERIMENTAL})
+    public static boolean movableBlockOverrides = false;
+
     @Rule(category = SURVIVAL)
     public static boolean stackableShulkerBoxes = false;
 
