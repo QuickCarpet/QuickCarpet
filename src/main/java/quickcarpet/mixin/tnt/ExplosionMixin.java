5a46b2488a64b2dab1c6b076d6bcb0fbfccf8bc9
Simon Wanner <skyrising@pvpctutorials.de>
2020-03-03 23:12:51 +0100

    Avoid lithium fast_explosions conflict

--- src/main/java/quickcarpet/mixin/tnt/ExplosionMixin.java
+++ src/main/java/quickcarpet/mixin/tnt/ExplosionMixin.java
@@ -1,38 +1,21 @@
 package quickcarpet.mixin.tnt;
 
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.explosion.Explosion;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import quickcarpet.annotation.Feature;
 import quickcarpet.settings.Settings;
-
-import java.util.Collection;
-import java.util.List;
-import java.util.Set;
 
 @Feature("explosionNoBlockDamage")
 @Mixin(Explosion.class)
 public abstract class ExplosionMixin {
-    @Shadow @Final private List<BlockPos> affectedBlocks;
-    
-    @Redirect(method = "collectBlocksAndDamageEntities", at = @At(value = "INVOKE",
-            target = "Ljava/util/List;addAll(Ljava/util/Collection;)Z", remap = false))
-    private boolean cancelAffectedBlocks(List list, Collection<?> c) {
-        return false;
-    }
-    
-    @Inject(method = "collectBlocksAndDamageEntities", at = @At(value = "INVOKE",
-            target = "Ljava/util/List;addAll(Ljava/util/Collection;)Z", remap = false), locals = LocalCapture.CAPTURE_FAILHARD)
-    private void newAffectedBlocks(CallbackInfo ci, Set<BlockPos> set_1) {
-        if (!Settings.explosionNoBlockDamage) {
-            this.affectedBlocks.addAll(set_1);
-        }
+    @Shadow public abstract void clearAffectedBlocks();
+
+    @Inject(method = "affectWorld", at = @At("HEAD"))
+    private void explosionNoBlockDamage(boolean bl, CallbackInfo ci) {
+        if (Settings.explosionNoBlockDamage) clearAffectedBlocks();
     }
 }
