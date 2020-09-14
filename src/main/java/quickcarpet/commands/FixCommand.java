b5169a412cc0fd2803389f4d3762e78a06acf368
Simon Wanner <skyrising@pvpctutorials.de>
2020-09-14 13:15:18 +0200

    Fixed `/fix` command freezing the server

--- src/main/java/quickcarpet/commands/FixCommand.java
+++ src/main/java/quickcarpet/commands/FixCommand.java
@@ -7,6 +7,7 @@ import net.minecraft.command.argument.BlockPosArgumentType;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.world.ServerLightingProvider;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.math.ColumnPos;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.Heightmap;
@@ -19,23 +20,29 @@ import static net.minecraft.command.argument.ColumnPosArgumentType.columnPos;
 import static net.minecraft.command.argument.ColumnPosArgumentType.getColumnPos;
 import static net.minecraft.server.command.CommandManager.argument;
 import static net.minecraft.server.command.CommandManager.literal;
+import static quickcarpet.utils.Messenger.m;
+import static quickcarpet.utils.Messenger.t;
 
 public class FixCommand {
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
         LiteralArgumentBuilder<ServerCommandSource> fix = literal("fix")
             .requires(s -> s.hasPermissionLevel(Settings.commandFix))
-            .then(literal("chunk")).then(argument("pos", columnPos())
-                .executes(c -> fixChunk(c.getSource(), getColumnPos(c, "pos"))));
+            .then(literal("chunk").then(argument("block_pos", columnPos())
+                .executes(c -> fixChunk(c.getSource(), getColumnPos(c, "block_pos")))));
         dispatcher.register(fix);
     }
 
     private static int fixChunk(ServerCommandSource source, ColumnPos pos) throws CommandSyntaxException {
         ServerWorld world = source.getWorld();
-        BlockView chunk = world.getExistingChunk(pos.x >> 4, pos.z >> 4);
+        ChunkPos cPos = new ChunkPos(pos.x >> 4, pos.z >> 4);
+        BlockView chunk = world.getExistingChunk(cPos.x, cPos.z);
         if (!(chunk instanceof WorldChunk)) throw BlockPosArgumentType.UNLOADED_EXCEPTION.create();
         WorldChunk worldChunk = (WorldChunk) chunk;
+        m(source, t("command.fix.fixing", cPos.x, cPos.z));
         Heightmap.populateHeightmaps(worldChunk, EnumSet.allOf(Heightmap.Type.class));
-        ((ServerLightingProvider) world.getLightingProvider()).light(worldChunk, worldChunk.isLightOn()).join();
+        ((ServerLightingProvider) world.getLightingProvider()).light(worldChunk, false).thenRun(() -> {
+            m(source, t("command.fix.fixed", cPos.x, cPos.z));
+        });
         return 1;
     }
 }
