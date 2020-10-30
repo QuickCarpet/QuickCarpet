9d14e652bb9dbfb4d4f4fee880184deafa441d89
Simon Wanner <skyrising@pvpctutorials.de>
2020-10-30 18:11:28 +0100

    Fixed `creativeNoClip` players getting pushed by the world

---
+++ src/main/java/quickcarpet/mixin/creativeNoClip/PistonBlockEntityMixin.java
@@ -0,0 +1,18 @@
+package quickcarpet.mixin.creativeNoClip;
+
+import net.minecraft.block.entity.PistonBlockEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import quickcarpet.utils.Utils;
+
+@Mixin(PistonBlockEntity.class)
+public class PistonBlockEntityMixin {
+    @Redirect(method = "pushEntities", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;setVelocity(DDD)V"))
+    private void setVelocity(Entity entity, double x, double y, double z) {
+        if (entity instanceof PlayerEntity && Utils.isNoClip(((PlayerEntity) entity))) return;
+        entity.setVelocity(x, y, z);
+    }
+}
--- src/main/java/quickcarpet/mixin/creativeNoClip/PlayerEntityMixin.java
+++ src/main/java/quickcarpet/mixin/creativeNoClip/PlayerEntityMixin.java
@@ -1,15 +1,30 @@
 package quickcarpet.mixin.creativeNoClip;
 
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.MovementType;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import quickcarpet.utils.Utils;
 
 @Mixin(PlayerEntity.class)
-public class PlayerEntityMixin {
+public abstract class PlayerEntityMixin extends LivingEntity {
+    protected PlayerEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
     @Redirect(method = {"tick", "updateSize"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;isSpectator()Z"))
     private boolean noClip(PlayerEntity player) {
         return Utils.isNoClip(player);
+    }
+
+    @Override
+    public void move(MovementType type, Vec3d movement) {
+        if (type != MovementType.SELF && Utils.isNoClip((PlayerEntity) (Object) this)) return;
+        super.move(type, movement);
     }
 }
