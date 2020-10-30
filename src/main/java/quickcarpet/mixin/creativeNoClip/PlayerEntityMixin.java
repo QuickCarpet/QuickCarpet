f0e588932f12e763c23cd889a890543887d7a60c
Simon Wanner <skyrising@pvpctutorials.de>
2020-10-30 17:48:16 +0100

    Fixed item pickup for `creativeNoClip`

--- src/main/java/quickcarpet/mixin/creativeNoClip/PlayerEntityMixin.java
+++ src/main/java/quickcarpet/mixin/creativeNoClip/PlayerEntityMixin.java
@@ -8,7 +8,7 @@ import quickcarpet.utils.Utils;
 
 @Mixin(PlayerEntity.class)
 public class PlayerEntityMixin {
-    @Redirect(method = {"tick", "tickMovement", "updateSize"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;isSpectator()Z"))
+    @Redirect(method = {"tick", "updateSize"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;isSpectator()Z"))
     private boolean noClip(PlayerEntity player) {
         return Utils.isNoClip(player);
     }
