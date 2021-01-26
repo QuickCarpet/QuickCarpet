7bbcd4032cfb156aecc4d7c5994770879dc26303
Simon Wanner <skyrising@pvpctutorials.de>
2021-01-26 23:54:37 +0100

    fix: cactus counter crashing clients

--- src/main/java/quickcarpet/mixin/hopperCounters/ItemEntityMixin.java
+++ src/main/java/quickcarpet/mixin/hopperCounters/ItemEntityMixin.java
@@ -1,8 +1,11 @@
 package quickcarpet.mixin.hopperCounters;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,12 +16,16 @@ import quickcarpet.helper.HopperCounter;
 import quickcarpet.settings.Settings;
 
 @Mixin(ItemEntity.class)
-public abstract class ItemEntityMixin {
+public abstract class ItemEntityMixin extends Entity {
     @Shadow public abstract ItemStack getStack();
+
+    public ItemEntityMixin(EntityType<?> type, World world) {
+        super(type, world);
+    }
 
     @Inject(method = "damage", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/ItemEntity;remove()V"))
     private void countCactus(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
-        if (Settings.hopperCounters && source == DamageSource.CACTUS) {
+        if (!world.isClient() && Settings.hopperCounters && source == DamageSource.CACTUS) {
             HopperCounter.getCounter(HopperCounter.Key.CACTUS).add(QuickCarpetServer.getMinecraftServer(), getStack());
         }
     }
