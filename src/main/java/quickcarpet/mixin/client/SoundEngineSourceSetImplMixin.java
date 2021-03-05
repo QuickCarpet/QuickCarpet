186ef5ddce07c85a48a5b76f6cbdd9d266c0a407
Simon Wanner <skyrising@pvpctutorials.de>
2021-03-05 20:31:43 +0100

    fix: mixin target createSource not found

--- src/main/java/quickcarpet/mixin/client/SoundEngineSourceSetImplMixin.java
+++ src/main/java/quickcarpet/mixin/client/SoundEngineSourceSetImplMixin.java
@@ -8,7 +8,7 @@ import quickcarpet.client.ClientSetting;
 
 @Mixin(targets = "net.minecraft.client.sound.SoundEngine$SourceSetImpl")
 public class SoundEngineSourceSetImplMixin {
-    @Redirect(method = "createSource", at = @At(value = "INVOKE", target = "Lorg/apache/logging/log4j/Logger;warn(Ljava/lang/String;Ljava/lang/Object;)V", remap = false))
+    @Redirect(method = "createSource()Lnet/minecraft/client/sound/Source;", at = @At(value = "INVOKE", target = "Lorg/apache/logging/log4j/Logger;warn(Ljava/lang/String;Ljava/lang/Object;)V", remap = false))
     private void skipLog(Logger logger, String message, Object arg) {
         if (ClientSetting.SOUND_ENGINE_FIX.get()) return;
         logger.warn(message, arg);
