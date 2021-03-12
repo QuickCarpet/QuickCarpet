051f13acad352ea85bcc736b3c9b1e3a7e79ddaa
Simon Wanner <skyrising@pvpctutorials.de>
2021-03-12 16:32:40 +0100

    fix: support ModMenu v1

--- src/main/java/quickcarpet/client/ModMenuEntry.java
+++ src/main/java/quickcarpet/client/ModMenuEntry.java
@@ -1,7 +1,7 @@
 package quickcarpet.client;
 
-import com.terraformersmc.modmenu.api.ConfigScreenFactory;
-import com.terraformersmc.modmenu.api.ModMenuApi;
+import io.github.prospector.modmenu.api.ConfigScreenFactory;
+import io.github.prospector.modmenu.api.ModMenuApi;
 
 public class ModMenuEntry implements ModMenuApi {
     @Override
