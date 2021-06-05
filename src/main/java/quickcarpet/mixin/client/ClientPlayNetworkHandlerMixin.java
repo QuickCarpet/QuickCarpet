950cb7e9d705f10c471434ea9ef133cfda3c401b
Simon Wanner <skyrising@pvpctutorials.de>
2021-06-05 16:12:39 +0200

    network: "incomplete tags" message again

--- src/main/java/quickcarpet/mixin/client/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/quickcarpet/mixin/client/ClientPlayNetworkHandlerMixin.java
@@ -10,6 +10,8 @@ import net.minecraft.network.packet.s2c.play.CustomPayloadS2CPacket;
 import net.minecraft.network.packet.s2c.play.SynchronizeTagsS2CPacket;
 import net.minecraft.tag.TagManager;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryKey;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -52,7 +54,7 @@ public abstract class ClientPlayNetworkHandlerMixin implements ClientPlayPacketL
     }
 
     @Inject(method = "onSynchronizeTags", at = @At(value = "INVOKE", target = "Lcom/google/common/collect/Multimap;isEmpty()Z", remap = false), locals = LocalCapture.CAPTURE_FAILHARD)
-    private void ignoreMissingCarpetTags(SynchronizeTagsS2CPacket packet, CallbackInfo ci, TagManager manager, Multimap<Identifier, Identifier> map) {
-        map.get(new Identifier("block")).removeAll(CarpetRegistry.CARPET_BLOCK_TAGS);
+    private void ignoreMissingCarpetTags(SynchronizeTagsS2CPacket packet, CallbackInfo ci, TagManager manager, Multimap<RegistryKey<?>, Identifier> map) {
+        map.get(Registry.BLOCK_KEY).removeAll(CarpetRegistry.CARPET_BLOCK_TAGS);
     }
 }
