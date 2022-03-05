df3baa106ef165a66788cc9b3ed32e03fc86b222
Simon Wanner <skyrising@pvpctutorials.de>
2022-03-05 10:58:23 +0100

    accurateBlockPlacement: Disable distance check introduced in 22w06a

---
+++ src/main/java/quickcarpet/mixin/accurateBlockPlacement/ServerPlayNetworkHandlerMixin.java
@@ -0,0 +1,16 @@
+package quickcarpet.mixin.accurateBlockPlacement;
+
+import net.minecraft.server.network.ServerPlayNetworkHandler;
+import net.minecraft.util.math.Vec3d;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import quickcarpet.settings.Settings;
+
+@Mixin(ServerPlayNetworkHandler.class)
+public class ServerPlayNetworkHandlerMixin {
+    @Redirect(method = "onPlayerInteractBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/Vec3d;subtract(Lnet/minecraft/util/math/Vec3d;)Lnet/minecraft/util/math/Vec3d;"))
+    private Vec3d quickcarpet$accurateBlockPlacement$removeHitPosCheck(Vec3d hitVec, Vec3d blockCenter) {
+        return Settings.accurateBlockPlacement ? Vec3d.ZERO : hitVec.subtract(blockCenter);
+    }
+}
