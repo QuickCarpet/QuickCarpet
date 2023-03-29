9593c692cbd151666e447f68f22113028c4122c3
Simon Wanner <simon+git@skyrising.xyz>
2023-03-29 22:54:20 +0200

    Fix incorrect method signature in DefaultLanguageMixin

--- src/main/java/quickcarpet/mixin/core/DefaultLanguageMixin.java
+++ src/main/java/quickcarpet/mixin/core/DefaultLanguageMixin.java
@@ -10,7 +10,7 @@ import java.util.Map;
 
 @Mixin(targets = "net.minecraft.util.Language$1")
 public abstract class DefaultLanguageMixin extends Language {
-    @Redirect(method = "get(Ljava/lang/String;)Ljava/lang/String;", at = @At(value = "INVOKE", target = "Ljava/util/Map;getOrDefault(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;", remap = false), require = 1, allow = 1)
+    @Redirect(method = "get(Ljava/lang/String;Ljava/lang/String;)Ljava/lang/String;", at = @At(value = "INVOKE", target = "Ljava/util/Map;getOrDefault(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;", remap = false), require = 1, allow = 1)
     private Object quickcarpet$get(Map<String, String> map, Object key, Object defaultValue) {
         //noinspection SuspiciousMethodCalls
         Object value = map.get(key);
